7fb1514e730d1a3e9f891c4c7de9397297b361d9
antoninaorobets <pani.antonina.don@gmail.com>
2022-05-30 12:39:06 -0400

    get user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,9 @@
+
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
-  end
-
-  post "/create" do
-    Job.create_job(params[:code])
-  end
-
-  post "/apply" do
-    job = Job.create_job(params[:code])
-    Application.create(user: params[:user_id], job: job, status: "applied")
+  get "/user" do
+    User.first.to_json
   end
 
 end
