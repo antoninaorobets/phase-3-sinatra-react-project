d9f55f1b1f8afc9d284b67415af77a89e5dfb280
antoninaorobets <pani.antonina.don@gmail.com>
2022-05-30 12:35:55 -0400

    update linkedin scrapper

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,5 @@ class User < ActiveRecord::Base
     has_many :applications
     has_many :jobs, through: :applications
 
-    # def jobs
-    #     all =[]
-    #     self.applications.each do |application|
-
-    #     end
-    # end
     
 end
